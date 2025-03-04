811ef1ec79f7036edb0c051eb39c027fd2e06a2b
Raimundas Bosikis <Rai@iMacas.local>
2025-03-04 11:12:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+#
+#
+#
+# import random
+#
+# vinies_ilgis = 85  # mm
+#
+# # Funkcija mažiems smūgiams
+# def mazi_smugiai():
+#     vinies_ikala = 0
+#     smugiai = 0
+#     while vinies_ikala < vinies_ilgis:
+#         smugis = random.randint(5, 20)  # atsitiktinis smūgio gylis (5-20 mm)
+#         vinies_ikala += smugis
+#         smugiai += 1
+#     return smugiai
+#
+# # Funkcija dideliems smūgiams
+# def dideli_smugiai():
+#     vinies_ikala = 0
+#     smugiai = 0
+#     while vinies_ikala < vinies_ilgis:
+#         if random.random() < 0.5:  # 50% tikimybė, kad smūgis pataikys
+#             smugis = random.randint(20, 30)  # atsitiktinis smūgio gylis (20-30 mm)
+#             vinies_ikala += smugis
+#         smugiai += 1
+#     return smugiai
+#
+# # Simuliuojame mažus smūgius
+# print("Maži smūgiai:")
+# mazu_smugiu_rezultatas = mazi_smugiai()
+# print(f"Smūgių skaičius mažiems smūgiams: {mazu_smugiu_rezultatas}")
+#
+# # Simuliuojame didelius smūgius
+# print("\nDideli smūgiai:")
+# dideli_smugiu_rezultatas = dideli_smugiai()
+# print(f"Smūgių skaičius dideliems smūgiams: {dideli_smugiu_rezultatas}")
+
